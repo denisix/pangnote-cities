fb1306b2c7978be341bfe98f8cb0f1a391a3a10b
Den <den@vdsinside.com>
2018-09-14 19:47:08 +0300

    - fixed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var pbf = new Pbf(fs.readFileSync(path.join(__dirname, 'cities.pbf')))
 var cities = []
 var lastLat = 0
 var lastLng = 0
+var countriesKeys = {}
 
 while (pbf.pos < pbf.length) {
     cities.push(pbf.readMessage(readCity, {
@@ -17,7 +18,7 @@ while (pbf.pos < pbf.length) {
     }))
 }
 
-module.exports = cities
+//module.exports = cities
 
 function readCity(tag, city, pbf) {
 	switch(tag) {
@@ -28,3 +29,27 @@ function readCity(tag, city, pbf) {
 		case 5: lastLng += pbf.readSVarint(); city.lng = lastLng / 1e5; break;
     }
 }
+
+function getCountriesKeys() {
+	if (Object.keys(countriesKeys).length > 0) {
+		return countriesKeys;
+	}
+
+	cities.map(i => countriesKeys[i.country]=null)
+	return countriesKeys;
+}
+
+function getCountryCities(c) {
+	let hash = {};
+	cities.filter(i => i.country == c).map(i => countriesKeys[i.country]=null)
+	return hash;
+}
+
+function getISOCities(c) {
+	let hash = {};
+	cities.filter(i => i.iso == c).map(i => countriesKeys[i.country]=null)
+	return hash;
+}
+
+export { cities, getCountriesKeys, getCountryCities, getISOCities };
+
